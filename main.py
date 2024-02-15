a192996b9199daebd3076d2c9f367007521748bb
Rebecca Dias <rebeccadias2510@gmail.com>
2024-02-14 19:21:35 -0800

    Cleared Unwanted Server Code

--- main.py
+++ main.py
@@ -88,28 +88,5 @@ def get_stock_news():
         return jsonify({"error": str(e)}), 500
 
 
-@app.route('/searchStockSummaryQuote')
-def search_quote():
-    ticker = request.args.get('ticker')
-    if not ticker:
-        return jsonify({'error': 'Ticker symbol is required'}), 400
-
-    response = requests.get(f"{FINNHUB_QUOTE_URL}?symbol={ticker}&token={FINNHUB_API_KEY}")
-    if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return jsonify({'error': 'Failed to fetch stock quote from Finnhub'}), response.status_code
-    
-@app.route('/searchRecommendationTrends/<tickerSymbol>')
-def search_recommendation_trends(tickerSymbol):
-    if not tickerSymbol:
-        return jsonify({'error': 'Ticker symbol is required'}), 400
-    
-    response = requests.get(f"{FINNHUB_RECOMMENDATION_TRENDS_URL}?symbol={tickerSymbol}&token={FINNHUB_API_KEY}")
-    if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return jsonify({'error': 'Failed to fetch company details from Finnhub'}), response.status_code
-
 if __name__ == '__main__':
     app.run(debug=True)
